57d07dca41507fa842d63b9dac75f496f01cd5e6
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2025-04-02 16:24:35 +0100

    Update parser.js

--- wikiScribbler/core/parser.js
+++ wikiScribbler/core/parser.js
@@ -11,67 +11,80 @@
  \ \_\    \ \_\ \_\  \ \_\ \_\  \/\_____\  \ \_____\  \ \_\ \_\
   \/_/     \/_/\/_/   \/_/ /_/   \/_____/   \/_____/   \/_/ /_/
 
-Version 1.0.0-rc.4
+Version 1.0.0-rc.5
 Made by Mimmeer
 */
 function parseChild(child, parentArray, parentObject = null) {
-  var elementData = { //create a new object
-    tag_name: child.tagName,
-    contents: "", // Start with empty content
-    children: [] // Array for children
+  var elementData = {
+    type: child.tagName, // Use 'type' instead of 'tag_name'
+    content: "", // Changed from 'contents' to 'content'
+    children: []
   };
 
-  if (child.nodeType === 3 && parentObject != null) { // 3 is Node.TEXT_NODE
-    if (child.textContent.trim() != null || child.textContent.trim() != "\n") {
-      parentObject.contents += child.textContent;
+  if (child.nodeType === 3) {
+    //handle text
+    if (parentObject != null) {
+      //if has parent
+      if (child.textContent.trim() != null && child.textContent.trim() != "\n") {
+        parentObject.children.push({  //add to parent's children
+            type: "sys.text",
+            content: child.textContent
+        });
+      }
+    } else {
+      //if no parent, make para
+      elementData.type = 'para';
+      elementData.content = child.textContent;
+      parentArray.push(elementData);
     }
-  }
-  else if (child.nodeType === 3) {
-    elementData.tag_name = 'para';
-    elementData.contents = child.textContent;
-  }
-
-  if (child.nodeType == 1) {
-	  parentArray.push(elementData); // Add to the parent
-	  for (let i = 0; i < child.childNodes.length; i++) { //childNodes
-    		parseChild(child.childNodes[i], elementData.children, elementData); // Recursive call
-  	  }
+  } else if (child.nodeType === 1) {
+    //handle element
+    parentArray.push(elementData);
+    if (child.tagName === "code") {
+      elementData.content = child.textContent;
+    } else {
+      for (let i = 0; i < child.childNodes.length; i++) {
+        parseChild(child.childNodes[i], elementData.children, elementData);
+      }
+    }
   }
 }
 
 function parse(xml) {
-	var rootElement = xml.getElementsByTagName("ws")[0];
-	var infoElement = rootElement.getElementsByTagName("info")[0];
-	var articleElement = rootElement.getElementsByTagName("article")[0];
-	var pageContents = [];
-	for (let i = 0; i < articleElement.childNodes.length; i++) {
-		var element = articleElement.childNodes[i];
-		parseChild(element, pageContents);
-	}
-	var title = infoElement.getElementsByTagName("title")[0];
-	var page = { "title": title.textContent, "contents": pageContents };
-	
-	console.log(page);
+  var rootElement = xml.getElementsByTagName("ws")[0];
+  var infoElement = rootElement.getElementsByTagName("info")[0];
+  var articleElement = rootElement.getElementsByTagName("article")[0];
+  var pageContents = [];
+  for (let i = 0; i < articleElement.childNodes.length; i++) {
+    const child = articleElement.children[i];
+    parseChild(child, pageContents);
+  }
+  var title = infoElement.getElementsByTagName("title")[0];
+  var page = {
+    title: title.textContent,
+    contents: pageContents
+  };
+
+  console.log(page);
+  return page;
 }
 
 // START OF RUN
 // This fetches the code, then parses it.
-fetch('./wiki/pages/index.xml') //  path to your WikiScriber file
+fetch('./wiki/pages/index.xml')
   .then(response => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    return response.text(); // Get the content of the file as text
+    return response.text();
   })
   .then(data => {
-    //  'data' will contain the content of your index.ws file
-    console.log(data);
-	var parser = new DOMParser();
-	var xmlDoc = parser.parseFromString(data,"text/xml");
-	const parsedData = parse(xmlDoc);
-    
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(data, "text/xml");
+    const parsedData = parse(xmlDoc);
+    console.log(parsedData);
   })
   .catch(error => {
-    //  error occurred during the fetch
     console.error('Fetch error:', error);
   });
+
